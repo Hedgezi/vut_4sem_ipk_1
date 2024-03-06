cce5193ec738a0b3d326c901fc81c321caa6f7eb
Mykola V <nikola@rootnix.net>
2024-03-06 17:52:22 +0100

    feat: add async user input processing

---
+++ Common/UserInputProcessing.cs
@@ -0,0 +1,48 @@
+namespace vut_ipk1.Common;
+
+public class UserInputProcessing(
+    IConnection _connection
+)
+{
+    public async Task ProcessUserInputAsync()
+    {
+        while (true)
+        {
+            var input = await Console.In.ReadLineAsync(); // TODO: Trim input
+        
+            if (input == null)
+                continue;
+        
+            if (input.StartsWith('/'))
+            {
+                var splitInput = input.Split(' ', 2);
+                var command = splitInput[0];
+                var arguments = splitInput.Length > 1 ? splitInput[1] : null;
+        
+                switch (command)
+                {
+                    case "/auth":
+                        if (arguments == null)
+                        {
+                            Console.WriteLine("Invalid command usage.");
+                            continue;
+                        }
+        
+                        var splitArguments = arguments.Split(' ');
+                        if (splitArguments.Length != 3)
+                        {
+                            Console.WriteLine("Invalid command usage.");
+                            continue;
+                        }
+        
+                        // connection.Auth(splitArguments[0], splitArguments[1], splitArguments[2]);
+        
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command.");
+                        break;
+                }
+            }
+        }
+    }
+}
