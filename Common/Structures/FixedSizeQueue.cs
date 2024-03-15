d50f7b8fcaf44b3e63dfafdab568a68b4df5cb90
Mykola V <nikola@rootnix.net>
2024-03-15 10:22:58 +0100

    feat: add fixed size queue for storing message ids

---
+++ Common/Structures/FixedSizeQueue.cs
@@ -0,0 +1,48 @@
+namespace vut_ipk1.Common.Structures;
+
+public class FixedSizeQueue<T>
+{
+    private readonly Queue<T> queue = new Queue<T>(); // Internal queue to store elements
+    private readonly int maxSize; // Maximum size of the queue
+
+    // Constructor takes the maximum size of the queue
+    public FixedSizeQueue(int maxSize)
+    {
+        if (maxSize <= 0)
+        {
+            throw new ArgumentException("Max size must be greater than 0", nameof(maxSize));
+        }
+        
+        this.maxSize = maxSize;
+    }
+
+    // Enqueue method with logic to maintain queue size
+    public void Enqueue(T item)
+    {
+        if (queue.Count >= maxSize)
+        {
+            queue.Dequeue(); // Remove the oldest element if the max size is reached
+        }
+        
+        queue.Enqueue(item); // Add the new item to the queue
+    }
+
+    // Dequeue method to remove and return the oldest element from the queue
+    public T Dequeue()
+    {
+        if (queue.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        
+        return queue.Dequeue();
+    }
+
+    // Property to get the current count of the queue
+    public int Count => queue.Count;
+
+    public bool Contains(T item)
+    {
+        return queue.Contains(item);
+    }
+}
