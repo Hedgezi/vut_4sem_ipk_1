b0e541e963edf132bf9372d20913703e6463cc8d
Mykola V <nikola@rootnix.net>
2024-03-31 01:01:49 +0100

    fix(udp): message parser exceptions and string conversion fix

--- Udp/Messages/UdpMessageParser.cs
+++ Udp/Messages/UdpMessageParser.cs
@@ -10,6 +10,9 @@ public static class UdpMessageParser
     public static (ushort messageId, bool result, ushort refMessageId, string messageContents) ParseReplyMessage(
         byte[] message)
     {
+        if (message[3] is not 0x00 and not 0x01)
+            throw new ArgumentException("Invalid result value in REPLY message.");
+        
         var messageId = BinaryPrimitives.ReadUInt16BigEndian(message.AsSpan()[1..3]);
         var result = message[3] == 0x01;
         var refMessageId = BinaryPrimitives.ReadUInt16BigEndian(message.AsSpan()[4..6]);
@@ -22,19 +25,26 @@ public static class UdpMessageParser
     {
         var messageId = BinaryPrimitives.ReadUInt16BigEndian(message.AsSpan()[1..3]);
         var displayName = ConvertAsciiBytesToString(message, 3);
-        var messageContents = ConvertAsciiBytesToString(message, 3 + displayName.Length + 1);
+        var messageContents = ConvertAsciiBytesToString(message, 3 + displayName.Length);
 
         return (messageId, displayName, messageContents);
     }
 
     private static string ConvertAsciiBytesToString(byte[] bytes, int startIndex)
     {
-        var shortByteArray = bytes[startIndex..];
+        byte[] shortByteArray;
+        if (bytes[startIndex] == 0x00)
+        {
+            startIndex++;
+            shortByteArray = bytes[startIndex..];
+        }
+        else
+        {
+            shortByteArray = bytes[startIndex..];
+        }
         var length = Array.IndexOf(shortByteArray, (byte)0x00);
-        length = length == -1 ? shortByteArray.Length - 1 : length + 1;
+        length = length == -1 ? shortByteArray.Length - 1 : length + 1; // TODO: Check if this is correct
 
-        var test = TextEncoding.GetString(shortByteArray, 0, length).Replace('\0', ' ').TrimEnd();
-
-        return TextEncoding.GetString(shortByteArray, 0, length).Replace('\0', ' ').TrimEnd();
+        return TextEncoding.GetString(shortByteArray, 0, length).Trim('\0');
     }
 }
