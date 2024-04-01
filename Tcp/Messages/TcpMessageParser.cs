daaec8d833422699f0e1bb8c2f3b8a35c9499104
Mykola V <nikola@rootnix.net>
2024-04-01 14:56:13 +0200

    fix(tcp): messages are case-insensitive

--- Tcp/Messages/TcpMessageParser.cs
+++ Tcp/Messages/TcpMessageParser.cs
@@ -7,7 +7,7 @@ public static class TcpMessageParser
 {
     public static (bool result, string messageContents) ParseReplyMessage(string message)
     {
-        var match = Regex.IsMatch(message, @"^REPLY (OK|NOK) IS [\x20-\x7E]{1,1400}$");
+        var match = Regex.IsMatch(message.ToUpper(), @"^REPLY (OK|NOK) IS [\x20-\x7E]{1,1400}$");
         if (!match)
             throw new ArgumentException("Invalid message format");
         
@@ -20,7 +20,7 @@ public static class TcpMessageParser
 
     public static (string displayName, string messageContents) ParseMsgMessage(string message)
     {
-        var match = Regex.IsMatch(message, @"^MSG FROM [\x21-\x7E]{1,20} IS [\x20-\x7E]{1,1400}$");
+        var match = Regex.IsMatch(message.ToUpper(), @"^MSG FROM [\x21-\x7E]{1,20} IS [\x20-\x7E]{1,1400}$");
         if (!match)
             throw new ArgumentException("Invalid message format");
         
@@ -34,7 +34,7 @@ public static class TcpMessageParser
     
     public static (string displayName, string messageContents) ParseErrMessage(string message)
     {
-        var match = Regex.IsMatch(message, @"^ERR FROM [\x21-\x7E]{1,20} IS [\x20-\x7E]{1,1400}$");
+        var match = Regex.IsMatch(message.ToUpper(), @"^ERR FROM [\x21-\x7E]{1,20} IS [\x20-\x7E]{1,1400}$");
         if (!match)
             throw new ArgumentException("Invalid message format");
         
